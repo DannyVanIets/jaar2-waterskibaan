c04db4bd655bb0b9abf9739f4378c0cda4b23fc0
Danny van Bokhorst <dannyvanbokhorst@live.nl>
2019-09-23 15:09:07 +0200

    Opdracht 1 code
    Alle code voor opdracht 1

---
+++ Classes.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    class Classes
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Sporter
+    {
+        public Sporter(List<IMoves> moves)
+        {
+
+        }
+
+        public int AantalRondenNogTeGaan()
+        {
+            int i = 0;
+        }
+
+        public Zwemvest Zwemvest()
+        {
+
+        }
+
+        public Skies Skies()
+        {
+            
+        }
+
+        //In de documentatie staat dat je color moet gebruiken als property, maar die heb ik niet?
+        public string KledingKleur()
+        {
+            return "Kleur van kleding";
+        }
+    }
+
+    class Zwemvest
+    {
+
+    }
+
+    class Skies
+    {
+
+    }
+
+    class Lijn
+    {
+        public void PositieOpKabel()
+        {
+
+        }
+    }
+}
---
+++ IMoves.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    interface IMoves
+    {
+        //In de documentatie heeft de move () erachter, maar geen idee wat daarmee wordt bedoeld.
+        int Move();
+    }
+}
