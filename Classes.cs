dd755a3fe9d277b1703a2695c1c08c1abe86dd45
Danny van Bokhorst <dannyvanbokhorst@live.nl>
2019-10-02 19:07:08 +0200

    Alles voor opdracht 3
    Was vergeten te uploaden...

--- Classes.cs
+++ Classes.cs
@@ -8,43 +8,19 @@ namespace Waterskibaan
 {
     class Classes
     {
-        static void Main(string[] args)
-        {
-            TestOpdracht2();
-        }
-        private static void TestOpdracht2()
-        {
-
-        }
+        
     }
 
     class Sporter
     {
+        public int AantalRondenNogTeGaan = 0;
+        public Zwemvest Zwemvest;
+        public Skies Skies;
+        public Color KledingKleur;
+        public Moves<IMoves> list = new Moves<IMoves>();
         public Sporter(List<IMoves> moves)
         {
 
-        }
-
-        public int AantalRondenNogTeGaan()
-        {
-            int i = 0;
-            return i;
-        }
-
-        public Zwemvest Zwemvest()
-        {
-            return null;
-        }
-
-        public Skies Skies()
-        {
-            return null;
-        }
-
-        //In de documentatie staat dat je color moet gebruiken als property, maar die heb ik niet?
-        public string KledingKleur()
-        {
-            return null;
         }
     }
 
@@ -60,107 +36,17 @@ namespace Waterskibaan
 
     class Lijn
     {
-        public void PositieOpKabel()
-        {
-
-        }
+        public int PositieOpDeKabel { get; set; }
     }
 
-    class Kabel
+    class Moves<T>
     {
-        private LinkedList<Lijn> _lijnen;
 
-        public Boolean IsStartPositieLeeg()
-        {
-            if (_lijnen.First.Value != null)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public void NeemLijnInGebruik(Lijn lijn)
-        {
-            if (_lijnen.First.Value != null)
-            {
-                _lijnen.AddFirst(lijn);
-            }
-        }
-
-        public void VerschuifLijnen()
-        {
-            if (_lijnen.Count == 9)
-            {
-                _lijnen.AddFirst(_lijnen.Last.Value);
-                _lijnen.RemoveLast();
-            }
-            else
-            {
-                //Moet nog functie bijkomen die er voor zorgt dat de posities worden aangepast!
-            }
-        }
-
-        public Lijn VerwijderLijnVanKabel()
-        {
-            if (_lijnen.Count >= 8)
-            {
-                return _lijnen.Last.Value;
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        public override string ToString()
-        {
-            if (_lijnen.Count == 0)
-            {
-                foreach (Lijn l in _lijnen)
-                {
-                    return $"{l}|";
-                }
-                return "";
-            }
-            else
-            {
-                return "";
-            }
-        }
     }
 
-    class LijnenVoorraad
+    //Replacement for Color, which isn't a type in a console application.
+    class Color
     {
-        private Queue<Lijn> _lijnen;
 
-        public void LijnToevoegenAanRij(Lijn lijn)
-        {
-
-        }
-
-        public Lijn VerwijderEersteLijn()
-        {
-            if (_lijnen.Count == 0)
-            {
-                return _lijnen.Dequeue();
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        public int GetAantalLijnen()
-        {
-            return 0;
-        }
-
-        public override string ToString()
-        {
-            return $"{_lijnen.Count} lijnen op voorraad";
-        }
     }
 }
---
+++ Testing.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    class Testing
+    {
+        static void Main(string[] args)
+        {
+            TestOpdracht2();
+            //TestOpdracht3();
+        }
+
+        private static void TestOpdracht3()
+        {
+            LijnenVoorraad lv1 = new LijnenVoorraad();
+            Lijn lijn1 = new Lijn();
+
+            //Hier testen we om te kijken of de ToString en GetAantalLijnen methodes goed werken zonder iets toe te voegen.
+            Console.WriteLine(lv1.ToString());
+
+            //Hier testen we het toevoegen van een lijn.
+            lv1.LijnToevoegenAanRij(lijn1);
+            Console.WriteLine(lv1.ToString());
+
+            //Hier testen we het toevoegen van 2de lijn.
+            Lijn lijn2 = new Lijn();
+            lv1.LijnToevoegenAanRij(lijn2);
+            Console.WriteLine(lv1.ToString());
+
+            //Hier testen we het verwijderen van de 1ste lijn.
+            lv1.VerwijderEersteLijn();
+            Console.WriteLine(lv1.ToString());
+        }
+
+        private static void TestOpdracht2()
+        {
+            Kabel kabel1 = new Kabel();
+            Lijn lijn1 = new Lijn();
+            Console.WriteLine(kabel1.ToString());
+
+            //Test om te zien of een lijn succesvol wordt toegevoegd en of de start positie leeg is.
+            Console.WriteLine(kabel1.IsStartPositieLeeg());
+            kabel1.NeemLijnInGebruik(lijn1);
+            Console.WriteLine(kabel1.IsStartPositieLeeg());
+            Console.WriteLine(kabel1.ToString());
+
+            //Test om te zien of het verschuiven goed gaat.
+            kabel1.VerschuifLijnen();
+            Console.WriteLine(kabel1.ToString());
+
+            //Test om te zien of een 2de lijn succesvol wordt toegevoegd
+            Lijn lijn2 = new Lijn();
+            kabel1.NeemLijnInGebruik(lijn2);
+            Console.WriteLine(kabel1.ToString());
+
+            //Test om te zien of het verschuiven van meerder lijnen goed gaat.
+            kabel1.VerschuifLijnen();
+            Console.WriteLine(kabel1.ToString());
+            kabel1.VerschuifLijnen();
+
+            //Kijken of het toevoegen van een 3de lijn goed gaat.
+            Lijn lijn3 = new Lijn();
+            kabel1.NeemLijnInGebruik(lijn3);
+            Console.WriteLine(kabel1.ToString());
+
+            //Hier verschuiven we de lijnen tot er een op de 9de positie is.
+            kabel1.VerschuifLijnen();
+            kabel1.VerschuifLijnen();
+            kabel1.VerschuifLijnen();
+            kabel1.VerschuifLijnen();
+            kabel1.VerschuifLijnen();
+            kabel1.VerschuifLijnen();
+            Console.WriteLine(kabel1.ToString());
+
+            //Hier testen we hoe de methode omgaat met een lijn op positie 9.
+            kabel1.VerschuifLijnen();
+            Console.WriteLine(kabel1.ToString());
+        }
+    }
+}
